efe6fb95ffef09f28e0f8ac6a32c1cb87f310968
kosemeci <kosemecialperen37@gmail.com>
2023-12-02 13:59:06 +0300

    Giris yap sayfasından post ile gelen verileri db ye aktarma kodu yazıldı.

--- baglan.php
+++ baglan.php
@@ -1,10 +1,8 @@
 <?php
-$baglanti = new mysqli("localhost", "root", "", "trip_database:v2");
+$baglanti = new mysqli("localhost", "root", "", "trip_database");
 
 if ($baglanti->connect_errno > 0) {
     die("<b>Bağlantı Hatası:</b> " . $baglanti->connect_error);
 }
-
-echo "MySQL bağlantısı başarıyla gerçekleştirildi.";
 
 ?>
--- uye_ol.php
+++ uye_ol.php
@@ -1,5 +1,5 @@
 <?php
-require_once("baglan.php");
+require("baglan.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // POST ile gelen verileri al
 
@@ -7,8 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $sifre = $_POST["sifre"];
     $sifre_tekrar = $_POST["sifre_tekrar"];
-    
-    if(preg_match("/^[a-zA-ZüÜğĞıİşŞçÇöÖ\s]+$/", $isim)){
+
+    if (preg_match("/^[a-zA-ZüÜğĞıİşŞçÇöÖ\s]+$/", $isim)) {
         echo "İsim: " . $isim . "<br>";
     }
 
@@ -16,16 +16,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "E-posta: " . $email . "<br>";
     }
     if (!(!preg_match("/[a-zA-Z]/", $sifre) || !preg_match("/\d/", $sifre) ||
-    !preg_match("/[\W_]/", $sifre) || strlen($sifre) < 8 )) {
+        !preg_match("/[\W_]/", $sifre) || strlen($sifre) < 8)) {
         echo "Sifre: " . $sifre . "<br>";
     }
-    if($sifre==$sifre_tekrar){
+    if ($sifre == $sifre_tekrar) {
         echo "Sifre Tekrarı: " . $sifre_tekrar . "<br>";
     }
-    $hashedPassword = password_hash($sifre, PASSWORD_DEFAULT);
+    $hashed_password = password_hash($sifre, PASSWORD_DEFAULT);
 
-    echo "Hash'lenmiş Şifre: " . $hashedPassword . "<br>";
-    
+    $premium_kontrol=0;
+    $kullanici_tipi=0;
+    $rota_sayac=0;
+
+    $sql = "INSERT INTO kullanici (e_mail, ad_soyad, p_hash,premium_kontrol,kullanici_tipi,rota_sayac) 
+    VALUES ('$email', '$isim', '$hashed_password','$premium_kontrol','$kullanici_tipi','$rota_sayac')";
+
+    // Sorguyu çalıştırma
+    if ($baglanti->query($sql) === TRUE) {
+        echo "Veri başarıyla eklendi.";
+    } else {
+        echo "Veri eklenirken hata oluştu: " . $baglanti->error;
+    }
+
 }
-
-?>
